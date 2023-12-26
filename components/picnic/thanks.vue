d4e8e57d46086a6a14920b6ad52a9ced6ff3db70
Fukahire <hoy1589@gmail.com>
2023-12-26 18:08:19 +0800

    gsap

--- components/picnic/thanks.vue
+++ components/picnic/thanks.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="grid grid-cols-3 lg:grid-cols-4 grid-flow-row gap-6 px-8">
-            <a class="flex-row flex w-full gap-8 justify-center group" target="_blank" v-for="item in thanksList" :href="item.url">
-                <div class="relative">
-                    <div class="rounded-full overflow-hidden w-28 bg-[#FFCD40] p-2"><img :src="`_nuxt/components/picnic/asset/${item.image}.jpg`" class="rounded-full" alt=""></div>
-                    <p class="bg-[#2E4187] px-2.5 py-1 rounded-lg font-semibold absolute bottom-0 right-0 rotate-6 text-white">{{item.name}}</p>
-                </div>
-            </a>
-        </div>
+        <a class="flex-row flex w-full gap-8 justify-center group" target="_blank" v-for="item in thanksList" :href="item.url">
+            <div class="relative">
+                <div class="rounded-full overflow-hidden w-28 bg-[#FFCD40] p-2"><img :src="`_nuxt/components/picnic/asset/${item.image}.jpg`" class="rounded-full" alt=""></div>
+                <p class="bg-[#2E4187] px-2.5 py-1 rounded-lg font-semibold absolute bottom-0 right-0 rotate-6 text-white">{{item.name}}</p>
+            </div>
+        </a>
+    </div>
 </template>
 <script setup>
 const thanksList = ref([
--- components/picnic/title.vue
+++ components/picnic/title.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-8 p-4 scroll-mt-8 flex justify-center items-center items-center mb-12 mt-16">
-            <span class="font-semibold text-white text-2xl py-2 px-8 border-[#e3e3e3] bg-[#6b21a8] rounded-lg">
-                <slot></slot>
-            </span>
-        </div>
+        <span class="font-semibold text-white text-2xl py-2 px-8 border-[#e3e3e3] bg-[#6b21a8] rounded-lg">
+            <slot></slot>
+        </span>
+    </div>
 </template>
