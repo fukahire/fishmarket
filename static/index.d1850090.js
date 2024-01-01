6baf2101e1cf22b8b6d9019918760f8ab628eb1a
fukahire <hoy1589@gmail.com>
2024-01-01 23:45:45 +0800

    Updates

--- static/index.d1850090.js
+++
@@ -1,1 +0,0 @@
-import{n as e,o as t,c as s,a as o}from"./entry.e3a9d782.js";const a=o("h1",{class:"title"},"Nuxt Gsap",-1),n=[a],l=e({__name:"index",setup(c){return(_,r)=>(t(),s("div",null,n))}});export{l as default};
