be1fd292d1cb4ac8b7be513efa87e475112ee96a
fukahire <hoy1589@gmail.com>
2024-01-24 00:57:13 +0800

    Updates

--- static/logo.06sVyt6Q.js
+++
@@ -1,1 +0,0 @@
-import"./entry.gCbTNiQw.js";const i=""+globalThis.__publicAssetsURL("visual/picnic-b/logo.png");export{i as _};
---
+++ static/logo.QgGn1I0Q.js
@@ -0,0 +1,1 @@
+import"./entry.HU89vz_M.js";const i=""+globalThis.__publicAssetsURL("visual/picnic-b/logo.png");export{i as _};
