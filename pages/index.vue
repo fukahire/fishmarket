dfbc9b006c510c672e1e60b269b90eda7ef436b5
fukahire <hoy1589@gmail.com>
2024-08-24 20:37:42 +0800

    debug

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="mx-auto flex flex-col items-center justify-center h-screen">
-        <h1 class="font-bold text-lg">斯普拉遁魚市場</h1>
-        <a href="https://discord.com/invite/VTjUQkB9uv">斯普拉遁魚市場 Discord</a>
-        <div class="flex flex-col md:flex-row gap-12 my-8">
+        <h1 class="font-bold text-2xl">斯普拉遁魚市場</h1>
+        <a class="mt-4 text-white bg-[#717277] hover:bg-indigo-700 py-1 px-3 text-center rounded-lg inline-block" href="https://discord.com/invite/VTjUQkB9uv">Discord</a>
+        <div class="flex flex-col md:flex-row gap-12 my-12">
             <hall v-for="e in eventName" :eventName="e"></hall>
         </div>
     </div>
