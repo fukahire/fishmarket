b9a3138333134b5fd9b08e684124406b139a3a30
fukahire <hoy1589@gmail.com>
2024-08-24 20:25:52 +0800

    add index

---
+++ components/hall.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="">
+    <NuxtLink class="group block w-24 relative" :to="`/${event}`" target="_blank">
+        <div class="absolute w-full h-full opacity-50 bg-slate-400 rotate-0 sm:group-hover:rotate-12 -z-10 ease-in-out transition"></div>
+        <nuxtImg :src="`/event_logo/${event}.jpg`" alt=""></nuxtImg>
+        <p class="text-center absolute bottom-0 translate-y-full pt-1 sm:pt-3 left-1/2 -translate-x-1/2 opacity-100 sm:opacity-0 sm:group-hover:opacity-100 transition sm:group-hover:pt-2">{{ event }}</p>
+    </NuxtLink>
+</div>
+</template>
+
+<script setup>
+const props = defineProps({
+    eventName: String,
+})
+
+const event = props.eventName
+console.log(event)
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <h1 class="title">斯普拉遁魚市場</h1>
+    <div class="mx-auto flex flex-col items-center justify-center h-screen">
+        <h1 class="font-bold text-lg">斯普拉遁魚市場</h1>
+        <a href="https://discord.com/invite/VTjUQkB9uv">斯普拉遁魚市場 Discord</a>
+        <div class="flex flex-col md:flex-row gap-12 my-8">
+            <hall v-for="e in eventName" :eventName="e"></hall>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    // const { $gsap } = useNuxtApp()
-
-    // onMounted(() => {
-    //     $gsap.to('.title', { rotation: 3, x: 100, duration: 1 })
-    // })
+const eventName = ref(['ikaintrouble3','picnic','picnic2'])
 </script>
