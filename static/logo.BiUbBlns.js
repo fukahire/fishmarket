f8e04f32d513020732c35e1de4c985e421e67f96
fukahire <hoy1589@gmail.com>
2026-09-19 23:41:10 +0800

    Updates

--- static/logo.BiUbBlns.js
+++
@@ -1,1 +0,0 @@
-import"./entry.W3pKXmUO.js";const i=""+globalThis.__publicAssetsURL("visual/picnic-b/logo.png");export{i as _};
---
+++ static/logo.QhLPjWtk.js
@@ -0,0 +1,1 @@
+import"./entry.sVIh4c5z.js";const i=""+globalThis.__publicAssetsURL("visual/picnic-b/logo.png");export{i as _};
